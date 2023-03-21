fe8e249353c3fec5c71f9fdaeb5655f65bef65e5
Timo Diep <tdiep@wisc.edu>
2023-03-20 21:42:14 -0500

    Fixed syntax errors

--- project/main/pc.v
+++ project/main/pc.v
@@ -5,28 +5,25 @@ module pc(
     //Inputs
     Imm,
     Rs,
-    j_form,
     jump,
     reg_jump,
     halt
 
 );
     input wire jump, reg_jump, halt;
-    input wire[15:0] Imm;
-    output wire[15:0] PcAddr, ReadAddr;
+    input wire[15:0] Imm, Rs;
+    output wire[15:0] PcAddr;
     
     wire [15:0] Inc2, AddrDisp, stage1, AddrRel, PcImm, RsImm;
 
-    cla16b PcInc(.sum(Inc2), .cOut(), .inA(PC), .inB(2), cIn.());
+    cla16b PcInc(.sum(Inc2), .cOut(), .inA(PC), .inB(2), .cIn());
     cla16b PImm(.sum(PcImm), .cOut(), .inA(Inc2), .inB(Imm), .cIn());
     cla16b RImm(.sum(RsImm), .cOut(), .inA(Rs), .inB(Imm), .cIn());
-
-    assign AddrDisp = j_form ? RsImm : PcImm;  
-    assign stage1 = jump ? AddrDisp : Inc2;
-
-    cla16b RsDisp(.sum(AddrRel), .cOut(), .inA(Rs), .inB(Imm), cIn.());
-
-    assign stage2 = reg_jump ? AddrRel : stage1;
+ 
+    cla16b RsDisp(.sum(AddrRel), .cOut(), .inA(Rs), .inB(Imm), .cIn());
+    
+    assign stage1 = jump ? PcImm : Inc2;
+    assign stage2 = reg_jump ? RsImm : stage1;
 
     assign PC = halt ? 0 : stage2;
 
