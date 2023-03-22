27588ea3e2ce987321ad9a7c343fa03bd12c7edf
Timo Diep <tdiep@wisc.edu>
2023-03-21 23:09:57 -0500

    Fixed logic errors

--- project/main/RegMem.v
+++ project/main/RegMem.v
@@ -21,8 +21,8 @@ module RegMem(
     input wire clk, rst;
     input wire LBI, Link;
     input wire[15:0] PC, Imm;
-    input wire[2:0] ReadReg1, ReadReg2, WriteReg, WriteData;
-    output reg[15:0] Reg1Data, Reg2Data;
+    input wire[2:0] ReadReg1, ReadReg2, WriteReg ;
+    output wire[15:0] Reg1Data, Reg2Data, WriteData;
 
     //Write Register address logic  <-- Currently only 3 inputs/options, need 4 (2 bit sel) b/c two Rd's
     // (Input, LinkReg[1:0]): (Instr[10:8] (Rs), 00) , (Instr[7:5] (Rd I1-form), 01) , and (R7, 10) , (Instr[4:2] (Rd R-form), 11)
@@ -33,15 +33,14 @@ module RegMem(
 
     //Write Register Data logic
     wire [15:0] data, PcSum2, ImmSel;
-    cla16b Pc2(.sum(PcSum2), .cOut(), .inA(PC), .inB(2), .cIn());
+    wire zero;
+    assign zero = 0;
+    cla16b Pc2(.sum(PcSum2), .cOut(), .inA(PC), .inB(2), .cIn(zero));
     assign ImmSel = LBI ? Imm : WriteData;
     assign data = Link ? PcSum2 : ImmSel ;
 
-    rf_bypass RegFile(
-                  // Outputs
-                  .read1OutData(Reg1Data), .read2OutData(Reg2Data), .err(),
-                  // Inputs
+    wire[15:0] out1, out2;
+    rf_bypass RegFile(.read1OutData(out1), .read2OutData(out2), .err(),
                   .clk(clk), .rst(rst), .read1RegSel(ReadReg1), .read2RegSel(ReadReg2),
-                  .writeRegSel(LinkReg), .writeInData(WriteData), .writeEn(1)
-                  );
+                  .writeRegSel(LinkReg), .writeInData(data), .writeEn(1'b1));
 endmodule
