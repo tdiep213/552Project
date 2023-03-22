303d0a162c3919cef1bbb201dd8331bd6a4003f4
Timo Diep <tdiep@wisc.edu>
2023-03-21 21:11:40 -0500

    Fixed alu logical errors after testing

--- project/main/alu.v
+++ project/main/alu.v
@@ -1,15 +1,35 @@
 //Arithmetic module
-module alu(out, opcode, Ain, Bin);
+module alu(out, opcode, funct, Ain, Bin);
     parameter OPERAND_WIDTH = 16;
 
-    output wire[15:0] out;
+    output reg[15:0] out;
     input wire[15:0] Ain, Bin;
     input wire[4:0]opcode;
     input wire[1:0]funct;
 
     wire[15:0] inv;
-    wire[15:0] s0, s1, s2;
+    reg[15:0] s0, s1, s2;
     wire ltcomp;
+    
+    wire zero;
+    assign zero = 0;
+
+    /* Arithmetic logic */
+    wire[15:0] sum, diff;
+    cla16b RegSum(.sum(sum), .cOut(), .inA(Ain), .inB(Bin), .cIn(zero));
+
+    TwosComp sub(.out(inv), .in(Ain));
+    cla16b RegSub(.sum(diff), .cOut(), .inA(Bin), .inB(inv), .cIn(zero));
+
+
+    /* Conditional logic */
+    wire[15:0] slt16, sle16, sCoSum;
+    lt16b lt(.out(slt16), .Ain(Ain), .Bin(Bin));
+
+    lt16b le(.out(ltcomp), .Ain(Ain), .Bin(Bin));
+    assign sle16 = ltcomp | (&(Ain==Bin));    
+
+    cla16b COSum(.sum(), .cOut(sCoSum), .inA(Ain), .inB(Bin), .cIn(zero));
 
 
     always @* begin 
@@ -41,7 +61,7 @@ module alu(out, opcode, Ain, Bin);
                         out = Ain^Bin;
                     end
                     2'b11: begin //NANDI
-                        out = ~(Ain&Bin);
+                        out = Ain&~(Bin);
                     end
                 endcase
             end
@@ -67,29 +87,29 @@ module alu(out, opcode, Ain, Bin);
             end
             3'b101: begin //Shift Immediate
                 case(opcode[1:0])
-                    2'b00: begin //SLL
+                    2'b01: begin //SLL
                         s0 = Bin[0] ? {Ain[OPERAND_WIDTH - 2 : 0], 1'h0} : Ain;   //Shift 1 
                         s1 = Bin[1] ? {s0[OPERAND_WIDTH - 3 : 0], 2'h0} : s0;     //Shift 2  
                         s2 = Bin[2] ? {s1[OPERAND_WIDTH - 5 : 0], 4'h0} : s1;     //Shift 4  
-                        Out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], 8'h0} : s2;    //Shift 8
+                        out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], 8'h0} : s2;    //Shift 8
                     end
-                    2'b01: begin //SRL
+                    2'b11: begin //SRL
                         s0 = Bin[0] ? {1'b0, Ain[OPERAND_WIDTH - 1 : 1]} : Ain;   //Shift 1
                         s1 = Bin[1] ? {2'h0, s0[OPERAND_WIDTH - 1 : 2]} : s0;     //Shift 2
                         s2 = Bin[2] ? {4'h0, s1[OPERAND_WIDTH - 1 : 4]} : s1;     //Shift 4
-                        Out = Bin[3] ? {8'h0, s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
+                        out = Bin[3] ? {8'h0, s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
                     end
-                    2'b10: begin //ROL
+                    2'b00: begin //ROL
                         s0 = Bin[0] ? {Ain[OPERAND_WIDTH - 2 : 0], Ain[OPERAND_WIDTH - 1]} : Ain;                     //Shift 1
                         s1 = Bin[1] ? {s0[OPERAND_WIDTH - 3 : 0], s0[OPERAND_WIDTH - 1: OPERAND_WIDTH - 2]} : s0;     //Shift 2
                         s2 = Bin[2] ? {s1[OPERAND_WIDTH - 5 : 0], s1[OPERAND_WIDTH - 1: OPERAND_WIDTH - 4]} : s1;     //Shift 4
-                        Out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], s2[OPERAND_WIDTH - 1: OPERAND_WIDTH - 8]} : s2;    //Shift 8
+                        out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], s2[OPERAND_WIDTH - 1: OPERAND_WIDTH - 8]} : s2;    //Shift 8
                     end
-                    2'b11: begin //ROR
+                    2'b10: begin //ROR
                         s0 = Bin[0] ? { Ain[0], Ain[OPERAND_WIDTH - 1 : 1]} : Ain;                    //Shift 1
                         s1 = Bin[1] ? { s0[OPERAND_WIDTH - 1:0], s0[OPERAND_WIDTH - 1 : 2]} : s0;     //Shift 2
                         s2 = Bin[2] ? { s1[OPERAND_WIDTH - 3: 0], s1[OPERAND_WIDTH - 1 : 4]} : s1;    //Shift 4
-                       Out = Bin[3] ? { s2[OPERAND_WIDTH - 7: 0], s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
+                       out = Bin[3] ? { s2[OPERAND_WIDTH - 7: 0], s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
                     end
                 endcase
             end
@@ -107,7 +127,7 @@ module alu(out, opcode, Ain, Bin);
                                 out = Ain^Bin;
                             end
                             2'b11: begin //NAND
-                                out = ~(Ain&Bin);
+                                out = Ain&~(Bin);
                             end
                         endcase
                     end
@@ -118,25 +138,25 @@ module alu(out, opcode, Ain, Bin);
                                 s0 = Bin[0] ? {Ain[OPERAND_WIDTH - 2 : 0], 1'h0} : Ain;       //Shift 1 
                                 s1 = Bin[1] ? {s0[OPERAND_WIDTH - 3 : 0], 2'h0} : s0;     //Shift 2  
                                 s2 = Bin[2] ? {s1[OPERAND_WIDTH - 5 : 0], 4'h0} : s1;     //Shift 4  
-                                Out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], 8'h0} : s2;    //Shift 8
+                                out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], 8'h0} : s2;    //Shift 8
                             end
                             2'b01: begin //SRL
                                 s0 = Bin[0] ? {1'b0, Ain[OPERAND_WIDTH - 1 : 1]} : Ain;       //Shift 1
                                 s1 = Bin[1] ? {2'h0, s0[OPERAND_WIDTH - 1 : 2]} : s0;     //Shift 2
                                 s2 = Bin[2] ? {4'h0, s1[OPERAND_WIDTH - 1 : 4]} : s1;     //Shift 4
-                                Out = Bin[3] ? {8'h0, s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
+                                out = Bin[3] ? {8'h0, s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
                             end
                             2'b10: begin //ROL
                                 s0 = Bin[0] ? {Ain[OPERAND_WIDTH - 2 : 0], Ain[OPERAND_WIDTH - 1]} : Ain;                           //Shift 1
                                 s1 = Bin[1] ? {s0[OPERAND_WIDTH - 3 : 0], s0[OPERAND_WIDTH - 1: OPERAND_WIDTH - 2]} : s0;     //Shift 2
                                 s2 = Bin[2] ? {s1[OPERAND_WIDTH - 5 : 0], s1[OPERAND_WIDTH - 1: OPERAND_WIDTH - 4]} : s1;     //Shift 4
-                                Out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], s2[OPERAND_WIDTH - 1: OPERAND_WIDTH - 8]} : s2;    //Shift 8
+                                out = Bin[3] ? {s2[OPERAND_WIDTH - 9 : 0], s2[OPERAND_WIDTH - 1: OPERAND_WIDTH - 8]} : s2;    //Shift 8
                             end
                             2'b11: begin //ROR
                                 s0 = Bin[0] ? { Ain[0], Ain[OPERAND_WIDTH - 1 : 1]} : Ain;                          //Shift 1
                                 s1 = Bin[1] ? { s0[OPERAND_WIDTH - 1:0], s0[OPERAND_WIDTH - 1 : 2]} : s0;     //Shift 2
                                 s2 = Bin[2] ? { s1[OPERAND_WIDTH - 3: 0], s1[OPERAND_WIDTH - 1 : 4]} : s1;    //Shift 4
-                                Out = Bin[3] ? { s2[OPERAND_WIDTH - 7: 0], s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
+                                out = Bin[3] ? { s2[OPERAND_WIDTH - 7: 0], s2[OPERAND_WIDTH - 1 : 8]} : s2;    //Shift 8
                             end
                         endcase
                     end
@@ -162,22 +182,6 @@ module alu(out, opcode, Ain, Bin);
         endcase
     end
 
-/* Arithmetic logic */
-wire[15:0] sum, diff;
-cla16b RegSum(.sum(sum), .cOut(), .inA(Ain), .inB(inv), .cIn());
-
-TwosComp sub(.out(inv), .in(Ain));
-cla16b RegSub(.sum(diff), .cOut(), .inA(Bin), .inB(inv), .cIn());
-
-
-/* Conditional logic */
-wire[15:0] slt16, sle16, sCoSum;
-lt16b lt(.out(slt16), .Ain(Ain), .Bin(Bin));
-
-lt16b le(.out(ltcomp), .Ain(Ain), .Bin(Bin));
-assign sle16 = ltcomp | (&(Ain==Bin));    
-
-cla16b COSum(.sum(), .cOut(sCoSum), .inA(Ain), .inB(Bin), .cIn());
 
 
 endmodule
--- project/main/support/adder/TwosComp.v
+++ project/main/support/adder/TwosComp.v
@@ -1,12 +1,15 @@
 `default_nettype none
-module TwosComp(comp, in);
-    output wire[15:0] comp;
+module TwosComp(out, in);
+    output wire[15:0] out;
     input wire[15:0] in;
 
     wire[15:0] inv;
+    wire zero;
 
+    assign zero = 0;
     assign inv = ~in;
-    cla16b inc(.sum(comp), .cOut(), .inA(inv), .inB(1), .cIn());
+
+    cla16b inc(.sum(out), .cOut(), .inA(inv), .inB(1), .cIn(zero));
 
 endmodule
 `default_nettype wire
