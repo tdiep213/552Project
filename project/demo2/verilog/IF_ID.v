92d6281197ce0bd16ceffd148137d1a000b4d729
Timo Diep <tdiep@wisc.edu>
2023-03-28 21:48:32 -0500

    Added IF/ID pipelining register and started connections in proc

---
+++ project/demo2/verilog/IF_ID.v
@@ -0,0 +1,37 @@
+module if_id(InstrOut, ImmExtOut, PcOut, InstrIn, ImmExtIn, PcIn, clk, rst,
+        LinkRegOut, DestRegSelOut, RegWriteOut, //Decode control
+        LinkRegIn, DestRegSelIn, RegWriteIn,
+        ALUSelOut, //Execute control
+        ALUSelIn,
+        MemEnableOut, MemWrOut, HaltOut, // Memory control
+        MemEnableIn, MemWrIn, HaltIn,
+        Val2RegOut, // Writeback control
+        Val2RegIn
+
+);
+    output wire[15:0] InstrOut, ImmExtOut, PcOut;
+    
+    input wire[15:0] InstrIn, ImmExt,In, PcIn;
+    input wire  LinkRegOut, DestRegSelOut, RegWriteOut, //Decode control
+                LinkRegIn, DestRegSelIn, RegWriteIn,
+                ALUSelOut, //Execute control
+                ALUSelIn,
+                MemEnableOut, MemWrOut, HaltOut, // Memory control
+                MemEnableIn, MemWrIn, HaltIn,
+                Val2RegOut, // Writeback control
+                Val2RegIn;
+    input wire clk, rst;
+
+
+    dff_16 Instruction(.q(InstrOut), .err(), .d(InstrIn), .clk(clk), .rst(rst));
+    dff_16 Immediate(.q(ImmExtOut), .err(), .d(ImmExtIn), .clk(clk), .rst(rst));
+    dff_16 ProgCnt(.q(PcOut), .err(), .d(PcIn), .clk(clk), .rst(rst));
+    
+    dff_16 ID_cntrl[2:0](.q({LinkRegOut, DestRegSelOut, RegWriteOut}), .err(), .d({LinkRegIn, DestRegSelIn, RegWriteIn}), .clk(clk) , .rst(rst));
+    dff_16 EX_cntrl(.q(ALUSelOut), .err(), .d(ALUSelIn), .clk(clk), .rst(rst));
+    dff_16 MEM_cntrl[2:0](.q({MemEnableOut, MemWrOut, HaltOut}), .err(), .d({MemEnableIn, MemWrIn, HaltIns}), .clk(clk), .rst(rst));
+    dff_16 WB_cntrl(.q(Val2RegOut), .err(), .d(Val2RegIn), .clk(clk), .rst(rst))
+
+    (.q(), .err(), .d(), .clk(clk), .rst(rst));
+
+endmodule
--- project/demo2/verilog/proc.v
+++ project/demo2/verilog/proc.v
@@ -3,76 +3,118 @@
 /* $Rev: 46 $ */
 `default_nettype none
 module proc (/*AUTOARG*/
-   // Outputs
-   err, 
-   // Inputs
-   clk, rst
-   );
+    // Outputs
+    err, 
+    // Inputs
+    clk, rst
+    );
 
-   input wire clk;
-   input wire rst;
+    input wire clk;
+    input wire rst;
 
-   output reg err;
+    output reg err;
 
-   // None of the above lines can be modified
+    // None of the above lines can be modified
 
-   // OR all the err ouputs for every sub-module and assign it as this
-   // err output
-   
-   // As desribed in the homeworks, use the err signal to trap corner
-   // cases that you think are illegal in your statemachines
-   
-   
-   /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
+    // OR all the err ouputs for every sub-module and assign it as this
+    // err output
+
+    // As desribed in the homeworks, use the err signal to trap corner
+    // cases that you think are illegal in your statemachines
 
 
-   wire[2:0] ImmSel;
-   
-   wire[15:0] Instr, PC, ImmExt, Rs;
-   wire[15:0] Rt, Writeback;
+    /* your code here -- should include instantiations of fetch, decode, execute, mem and wb modules */
 
-   wire[15:0] ALUout;
-   wire[15:0] MEMout;
 
-   wire zero, sign;
+    wire[2:0] ImmSel;
 
-   //Control signals
-   wire[4:0] ALUcntrl;
-   wire RegJmp, Halt, PcSel;  //FETCH
-   wire RegWrite;   //DECODE
-   wire[1:0] LinkReg, DestRegSel;
-   wire SIIC;
+    wire[15:0] Instr, PC, ImmExt, Rs;
+    wire[15:0] Rt, Writeback;
+
+    wire[15:0] ALUout;
+    wire[15:0] MEMout;
+
+    wire zero, sign;
+
+    //Control signals
+    wire[4:0] ALUcntrl;
+    wire RegJmp, Halt, PcSel;  //FETCH
+    wire RegWrite;   //DECODE
+    wire[1:0] LinkReg, DestRegSel;
+    wire SIIC;
 
     wire[15:0] EPC, EPC_D;
 
-   wire ALUSel;               //EXECUTE
-   wire MemEnable, MemWr;     //MEMORY
-   wire Val2Reg;              //WRITEBACK
-   wire ctrlErr, ext_err;     //ERRORs
+    wire ALUSel;               //EXECUTE
+    wire MemEnable, MemWr;     //MEMORY
+    wire Val2Reg;              //WRITEBACK
+    wire ctrlErr, ext_err;     //ERRORs
 
-   /*-----FETCH-----*/
+    /*-----FETCH-----*/
+    wire[15:0] IF_Instr, IF_PC, IF_ImmExt; 
+    fetch F(.Instr(IF_Instr), .PC(IF_PC), .Imm(ImmExt), .Rs(Rs), .RegJmp(RegJmp), .Halt(Halt), .PcSel(PcSel), .SIIC(SIIC), .clk(clk), .rst(rst));
 
-   fetch F(.Instr(Instr), .PC(PC), .Imm(ImmExt), .Rs(Rs), .RegJmp(RegJmp), .Halt(Halt), .PcSel(PcSel), .SIIC(SIIC), .clk(clk), .rst(rst));
 
-   /*-----DECODE-----*/
+    /*---------------*/
 
-   decode D( .Reg1Data(Rs), .Reg2Data(Rt), .Instr(Instr), .Imm(ImmExt), .Writeback(Writeback),
-             .PC(PC), .LBI(LinkReg[0]), .Link(LinkReg[1]), .DestRegSel(DestRegSel), .en(RegWrite), .clk(clk), .rst(rst) );
+    /*-----IF/ID-----*/
+    wire[15:0] ID_Instr, ID_PC, ID_ImmExt;
+    wire ID_LinkReg, ID_DestRegSel, ID_RegWrite;
 
-   /*-----EXECUTE-----*/
+    if_id IF_ID_PIPE(
+        /*-----PIPELINE OUT-----*/
+        .InstrOut(ID_Instr), .ImmExtOut(ID_ImmExt), .PcOut(ID_PC),              //Data out
+        .LinkRegOut(ID_LinkReg), .DestRegSelOut(ID_DestRegSel),                 //Control out (Decode)
+                .RegWriteOut(ID_RegWrite),                        
+        .ALUSelOut(EX_ALUSel),                                                  //Control out (Execute)
+        .MemEnableOut(MEM_MemEnable), .MemWrOut(MEM_MemWr), .HaltOut(MEM_Halt), //Control out (Memory)
+        .Val2RegOut(WB_Val2Reg),                                                //Control out (Writeback)
 
-   execute X(.out(ALUout), .RsVal(Rs), .RtVal(Rt), .Imm(ImmExt), .ALUSel(ALUSel), .opcode(Instr[15:11]), .funct(Instr[1:0]));
+        /*-----PIPELINE IN-----*/
+        .InstrIn(IF_Instr), .ImmExtIn(IF_ImmExt), .PcIn(IF_PC),                 //Data in 
+        .LinkRegIn(LinkReg), .DestRegSelIn(DestRegSel), .RegWriteIn(RegWrite),  //Execute control//Control in (Decode)
+        .ALUSelIn(ALUSel),                                                      //Control in (Execute)
+        .MemEnableIn(MemEnable), .MemWrIn(MemWr), .HaltIn(Halt),                //Control in (Memory)
+        .Val2RegIn(Val2Reg),                                                    //Control in (Writeback)
 
-   /*-----MEMORY-----*/
-   
+        .clk(clk), .rst(rst));
+
+    /*---------------*/
+
+
+    /*-----DECODE-----*/
+    
+    decode D( .Reg1Data(Rs), .Reg2Data(Rt), .Instr(ID_Instr), .Imm(ID_ImmExt), .Writeback(Writeback),
+                .PC(ID_PC), .LBI(LinkReg[0]), .Link(LinkReg[1]), .DestRegSel(DestRegSel), .en(RegWrite), .clk(clk), .rst(rst) );
+    /*---------------*/
+
+    /*-----ID/EX-----*/
+
+
+    /*---------------*/
+
+    /*-----EXECUTE-----*/
+    execute X(.out(ALUout), .RsVal(Rs), .RtVal(Rt), .Imm(ImmExt), .ALUSel(ALUSel), .opcode(Instr[15:11]), .funct(Instr[1:0]));
+    /*---------------*/
+
+    /*-----EX/MEM-----*/
+
+    /*---------------*/
+
+    /*-----MEMORY-----*/
     memory M (.data_out(MEMout), .data_in(Rt), .addr(ALUout), .enable(MemEnable), .wr(MemWr), .createdump(), .Halt(Halt), .clk(clk), .rst(rst));
+    /*---------------*/
+
+    /*-----MEM/WB-----*/
+
+    /*---------------*/
 
     /*-----WRITEBACK-----*/
     wb W(.Writeback(Writeback), .ALUout(ALUout), .MEMout(MEMout), .Val2Reg(Val2Reg));
-
+    /*---------------*/
 
     /*-----CONTROL-----*/
-    sign_ext EXT(.out(ImmExt), .err(ext_err), .in(Instr), .zero_ext(ImmSel));
+    sign_ext EXT(.out(IF_ImmExt), .err(ext_err), .in(Instr), .zero_ext(ImmSel));
 
     assign sign = Rs[15];
     assign zero = &(Rs == 16'h0000);
@@ -87,7 +129,7 @@ module proc (/*AUTOARG*/
 
     always@* begin
         case({ctrlErr, ext_err})
-	    default: err =0; //ctrlErr | ext_err;
+        default: err =0; //ctrlErr | ext_err;
         endcase
     end
 
