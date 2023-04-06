8d3d9245aad06ba83dffd2288c7fb8ed763bddc8
hmkiff <kiffel@wisc.edu>
2023-04-06 18:23:56 -0500

    updating comments

--- project/demo2/verilog/control.v
+++ project/demo2/verilog/control.v
@@ -191,18 +191,16 @@ module control(
                     end
                     5'b10010: begin
                         ImmSel[2:0] = 3'b001;  // Do zero extend 8 bits. // SLBI Rs, immediate Rs <- (Rs << 8) | I(zero ext.)
-                        LinkReg[1:0]= 2'b01;   // Do Not LBI, Do Not Link
+                        LinkReg[1:0]= 2'b01;   // Do LBI, Do Not Link
                     end
                     default: ctrlErr = 1'b1;
                 endcase
             end
             5'b001??: begin 
                 SIIC            = 1'b0;
-                
                 Val2Reg         = 1'b0;        // Sometimes Care // Do transmit ALU output
                 ALUSel          = 1'b1;        // Sometimes Care // Do use the Immediate value in ALU
                 Halt            = 1'b0;        // Do Not halt
-                
                 DestRegSel[1:0] = 2'b10;       // Do use R7
                 ALUcntrl[4:0]   = 5'b01000;    // Pass ADDI Opcode
                 MemWr           = 1'b0;        // Do Not write to memory
