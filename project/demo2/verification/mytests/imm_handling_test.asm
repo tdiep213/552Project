ceba4de062e4a0625fee8fd66e6c25aee658660b
hmkiff <kiffel@wisc.edu>
2023-04-06 18:54:45 -0500

    added second test file

---
+++ project/demo2/verification/mytests/imm_handling_test.asm
@@ -0,0 +1,17 @@
+## This test will go through each size and extension of immediates
+
+## 5-bit imm, zero-ext
+## 5-bit imm, sign-ext
+
+## 8-bit imm, zero-ext
+## 8-bit imm, sign-ext
+
+## 11-bit imm, sign-ext
+
+## Operations will include immidate math operations,
+## Jumps, Memory, and Branches
+
+## First draft of test will have NOPs to prevent hazards
+## from breaking mem based operations
+
+## Later drafts will incorporate important hazard checks
