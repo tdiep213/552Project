fd0641d0ec80636c33ef46258958be4127abd51e
hmkiff <kiffel@wisc.edu>
2023-04-08 15:40:26 -0500

    fixed comments in other test asm files

--- project/demo2/verification/mytests/writeback_cycler_test.asm
+++ project/demo2/verification/mytests/writeback_cycler_test.asm
@@ -1,6 +1,4 @@
-## Tests writeback safety and register selection,
-## by cycling through each RegAddr option for writeback operations
-
-## can be expanded later to verify hazard protection works correctly.
-
-## Also indirectly tests some jump operations
+// Tests writeback safety and register selection,
+// by cycling through each RegAddr option for writeback operations
+// can be expanded later to verify hazard protection works correctly.
+// Also indirectly tests some jump operations
