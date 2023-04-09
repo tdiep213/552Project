c6fa056157f7d369d9518099357d28731f7364aa
hmkiff <kiffel@wisc.edu>
2023-04-08 23:41:47 -0500

    added imm_handling_test.asm

--- project/demo2/verification/mytests/imm_handling_test.asm
+++ project/demo2/verification/mytests/imm_handling_test.asm
@@ -9,3 +9,18 @@
 // First draft of test will have NOPs to prevent hazards
 // from breaking mem based operations
 // Later drafts will incorporate important hazard checks
+
+lbi r0, 0
+addi r1, r0, 15     // sign-ext 5 bit imm
+xori r2, r1, 10     // zero-ext 5 bit imm
+st r2, r1, 15       // memory access with sign-ext 5 bit imm
+beqz r0, .BLOC      // signed 8 bit branch
+
+.JLOC
+halt                // True end
+
+.BLOC:
+slbi r3, 64         // zero-ext 8 bit imm
+j .JLOC             // signed 11 bit imm
+halt                // this should never execute
+
--- project/demo2/verification/mytests/partial_ldst_test.asm
+++
@@ -1,5 +0,0 @@
-lbi r0, 0
-lbi r5, 43
-lbi r6, 43
-lbi r7, 43
-halt
