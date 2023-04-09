8002ba2ac6e623107a849a135efa0d4bb5c2531a
hmkiff <kiffel@wisc.edu>
2023-04-09 12:21:34 -0500

    fix writeback test

--- project/demo2/verification/mytests/writeback_cycler_test.asm
+++ project/demo2/verification/mytests/writeback_cycler_test.asm
@@ -8,11 +8,13 @@ lbi r1, 32
 lbi r3, 16
 
 add r4, r3, r2  // test Rd = Instr[4:2]
-subi r4, r4, 48 // test Rd = Instr[7:5]
+subi r4, r4, 16 // test Rd = Instr[7:5]
+subi r4, r4, 16
+subi r4, r4, 16
 st r1, r0, 0    // test Rd used as a value loc and Rs as part of an address
 
-jal r1, .JLOC   // jump and store PC+2 in R7
-stu r3, r0, 64  // use Rd as a source, and save mem address at Rs
+jal .JLOC   // jump and store PC+2 in R7
+stu r3, r0, 0  // use Rd as a source, and save mem address at Rs
 halt            // true end
 
 .JLOC:
