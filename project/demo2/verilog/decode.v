0f7f4236e131b734164c89b735bad9ef47e5d57f
hmkiff <kiffel@wisc.edu>
2023-04-10 11:25:31 -0500

    undoing jr_flag in decode

--- project/demo2/verilog/decode.v
+++ project/demo2/verilog/decode.v
@@ -18,18 +18,11 @@ module decode (Reg1Data, Reg2Data, PcSel, Instr, Imm, Writeback, PC, PCNOW, LBI,
 
    
    wire[2:0] Rs, Rt;
-   reg branch, jr_flag; 
+   reg branch; 
    
-   
+  
    assign Rs = Instr[10:8];
    assign Rt = Instr[7:5];
-
-   always @* begin
-      case(Instr[15:11])
-         5'b00101 | 5'b00111: jr_flag = 1'b1;
-         default: jr_flag = 1'b0;
-      endcase
-   end
 
 
     //Write Register Data logic
@@ -41,14 +34,14 @@ module decode (Reg1Data, Reg2Data, PcSel, Instr, Imm, Writeback, PC, PCNOW, LBI,
 
     cla16b Pc2(.sum(PcSum2), .cOut(), .inA(PC_instr), .inB(16'h0002), .cIn(1'b0));
     assign ImmSel = LBI ? Imm : Writeback;
-    assign WriteData = (Link | jr_flag) ? PcSum2 : ImmSel;      
+    assign WriteData = Link ? PcSum2 : ImmSel;      
 
    
 
    RegMem RegisterMem(.Reg1Data(Reg1Data),.Reg2Data(Reg2Data),
                      .ReadReg1(Rs), .ReadReg2(Rt),.WriteReg(WriteRegAddr), .WriteData(WriteData), 
    //                 //Rs                    //Rd                 //Rt
-                     .en((en | jr_flag)), .clk(clk), .rst(rst));
+                     .en(en), .clk(clk), .rst(rst));
     
     assign Sflag = Reg1Data[15];
     assign Zflag = &(Reg1Data == 16'h0000);
