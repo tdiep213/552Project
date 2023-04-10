f2105972d9eff49c8d1a797776728886b7bc88b0
hmkiff <kiffel@wisc.edu>
2023-04-09 21:15:57 -0500

    testing PCSEl signals

--- project/demo2/verilog/decode.v
+++ project/demo2/verilog/decode.v
@@ -18,7 +18,7 @@ module decode (Reg1Data, Reg2Data, PcSel, Instr, Imm, Writeback, PC, LBI, Link, 
 
    
    wire[2:0] Rs, Rt;
-   reg branch; 
+   reg branch, jump; 
    
   
    assign Rs = Instr[10:8];
@@ -42,7 +42,14 @@ module decode (Reg1Data, Reg2Data, PcSel, Instr, Imm, Writeback, PC, LBI, Link, 
     assign Zflag = &(Reg1Data == 16'h0000);
    
    assign branch_flag = ((Instr[15:13] == 3'b011) | b_flag) ? 1'b1 : 1'b0;
-   assign PcSel = (branch_flag & ~Halt) ? (branch) : 1'b0; 
+   assign PcSel = (branch_flag & ~Halt) ? (branch| jump) : 1'b0; 
+
+   always @* begin
+      case(Instr[15:13)
+         3'b001: jump = 1'b1;
+         default: jump = 1'b0;
+      endcase
+   end
 
    always @* begin
       case(Instr[12:11])
