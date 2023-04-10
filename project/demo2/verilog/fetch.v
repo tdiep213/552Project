672081540a6408e598f81cfe08f1f2e5e5d7eb23
hmkiff <kiffel@wisc.edu>
2023-04-10 18:57:39 -0500

    stall broke because the fix for JALR focused a lot on R7 transferring quickly, but now other regs are not triggering the same effect, but it's okay because other regs go faster

--- project/demo2/verilog/fetch.v
+++ project/demo2/verilog/fetch.v
@@ -56,7 +56,7 @@
    wire [1:0] ChkRegSel;
    reg [2:0] ChkRegAddr;
 
-    pc ProgCnt(.PcAddr(PcAddr),.PC(PC), .Imm(HDU_Imm), .BrnchImm(BrnchAddr), .Rs(Rs), .jmpPC(jmpPC), .PcSel(PcSel),.RegJmp(RegJmp),
+    pc ProgCnt(.PcAddr(PcAddr),.PC(PC), .Imm(HDU_Imm), .BrnchImm(BrnchAddr), .RsAddr(Instr[10:8]), .Rs(Rs), .jmpPC(jmpPC), .PcSel(PcSel),.RegJmp(RegJmp),
     .Halt(Halt), .PcStall(HazNOP), .SIIC(SIIC), .clk(clk), .rst(rst));
     memory2c InstrMem(.data_out(Instr), .data_in(), .addr(PC), .enable(1'b1), .wr(1'b0), 
                         .createdump(), .clk(clk), .rst(rst));
--- project/demo2/verilog/pc.v
+++ project/demo2/verilog/pc.v
@@ -5,32 +5,35 @@ module pc(
     PC,
     //Inputs
     Imm,BrnchImm,
-    Rs, jmpPC,
+    Rs, jmpPC, RsAddr,
     PcSel,RegJmp,Halt,SIIC, PcStall,//Control Signals
     clk, rst
 );
     input wire PcSel, RegJmp, Halt, SIIC, PcStall;
     input wire clk, rst;
-    input wire[15:0] Imm, Rs, BrnchImm, jmpPC;
+    input wire [2:0] RsAddr;
+    input wire[15:0] Imm, Rs, BrnchImm, jmpPC, jalrImm;
     output reg[15:0] PcAddr; //Next Instruction 
     output wire[15:0]  PC;   // PC used on the outside
     
     wire [15:0] Inc2, PcImm, RsImm, PcQ;
     wire zero;
     assign zero = 0;
-
+    
     cla16b PcInc(.sum(Inc2), .cOut(), .inA(PcQ), .inB(16'h0002), .cIn(zero));
     cla16b PImm(.sum(PcImm), .cOut(), .inA(PcQ), .inB(BrnchImm), .cIn(zero));
-    cla16b RImm(.sum(RsImm), .cOut(), .inA(jmpPC), .inB(Imm), .cIn(zero));
+    cla16b RImm(.sum(R7Imm), .cOut(), .inA(jmpPC), .inB(Imm), .cIn(zero));
+    cla16b RImm(.sum(RsImm), .cOut(), .inA(Rs), .inB(Imm), .cIn(zero));
     
     assign PC = PcQ;
     dff_16 PcReg(.q(PcQ), .err(), .d(PcAddr), .clk(clk), .rst(rst));
 
+    assign jalrImm = (RsAddr ~= 3'b111) ? RsImm : R7Imm;
 
     always @* begin 
         casex({PcSel, RegJmp, Halt, SIIC})
             4'b0000: PcAddr = PcStall ? PcQ : Inc2 ; //PC+2
-            4'b?100: PcAddr = PcStall ? PcQ : RsImm;//JR JALR        
+            4'b?100: PcAddr = PcStall ? PcQ : jalrImm;//JR JALR        
             4'b1000: PcAddr = PcImm;//J JAL Branch
             4'b???1: PcAddr = 2;    //Exception Handler
             default: PcAddr = PcQ; //Halt
