bb681f3968ef5419d523dae7567e7f2dfc75a1e7
hmkiff <kiffel@wisc.edu>
2023-04-10 13:30:22 -0500

    possible syntax problem preventing jr_flag from triggering

--- project/demo2/verilog/decode.v
+++ project/demo2/verilog/decode.v
@@ -23,7 +23,8 @@ module decode (Reg1Data, Reg2Data, PcSel, Instr, Imm, Writeback, PC, PCNOW, LBI,
    
    always @* begin
       case(Instr[15:11])
-         5'b00110, 5'b00111: jr_flag = 1'b1;
+         5'b00110: jr_flag = 1'b1;
+         5'b00111: jr_flag = 1'b1;
          default jr_flag = 1'b0;
       endcase
    end
