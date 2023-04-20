32f424baa9a0dc234578c09ff6ada0da2b984358
Timo Diep <tdiep@wisc.edu>
2023-04-20 17:01:01 -0500

    Started FSM

--- project/cache/cache_direct/verilog/dm_fsm.v
+++ project/cache/cache_direct/verilog/dm_fsm.v
@@ -1,2 +1,73 @@
-module dm_fsm();
+module dm_fsm(  // Outputs
+            mem_addr, mem_wr, mem_rd, cache_en, cache_tag, cache_index,
+            offset, cache_data, cache_wr, comp, valid_in, sel,
+            // Inputs
+               //PROC
+            addr,
+            data,
+            rd,
+            wr,
+               //MEM
+            stall,
+            busy,
+               //CACHE
+            hit,
+            dirty,
+            valid
+            );
+
+
+    output wire [15:0] mem_addr, 
+                       cache_data;
+    output wire [7:0]  cache_index;
+    output wire [4:0] cache_tag;
+    output wire [2:0] offset;
+    output wire cache_wr, 
+                comp, 
+                valid_in, 
+                sel, 
+                cache_en, 
+                mem_wr, 
+                mem_rd, 
+                cache_en;
+
+    input wire [15:0] addr,
+                      data;
+    input wire rd, 
+               wr, 
+               stall, 
+               busy,
+               hit,
+               dirty,
+               valid;
+
+
+    assign cache_tag = addr[15:11];
+    assign cache_index = addr[10:3];
+    assign cache_tag = addr[2:0]; 
+
+    reg [3:0] state;
+    /* State list
+    0x0/default = rst/idle state
+    0x1 = Check cache
+
+
+
+    */
+    always @* begin 
+        cache_wr = 1'b0;
+        mem_rd = 1'b0;
+        mem_wr = 1'b0;
+        cache_en = 1'b0;
+        comp = 1'b0;
+        case(state)
+            default: begin // Default/Idle case
+                state = rd | wr ?  4'h1 : 4'h0;
+            end
+            4'h1: begin // Check cache  
+                cache_en = 1'b1;
+                comp = 1'b1;
+            end
+        endcase 
+    end 
 endmodule
--- project/cache/cache_direct/verilog/mem_system.v
+++ project/cache/cache_direct/verilog/mem_system.v
@@ -28,33 +28,36 @@ module mem_system(/*AUTOARG*/
     * needed for cache parameter */
    parameter memtype = 0;
 
-   wire mem_rd, mem_wr; 
+   wire mem_rd, mem_wr, busy; 
    wire[15:0] mem_addr, mem_data_out;
 
    wire cache_en, write, valid_in, comp;
    wire[15:0] cache_data_out, cache_data_in;
+
    wire[7:0] cache_index;
+   wire[4:0] cache_tag;
    wire[2:0] offset; 
 
+   wire sel;
 
    dm_fsm f0(  // Outputs
             .mem_addr(mem_addr), .mem_wr(mem_wr), .mem_rd(mem_rd),
             .cache_en(cache_en), .cache_tag(cache_tag), .cache_index(cache_index),
-            .offset(offset), .cache_data(cache_data_in), cache_wr(write),
-            .comp(comp), .valid_in(valid_in),
+            .offset(offset), .cache_data(cache_data_in), .cache_wr(write),
+            .comp(comp), .valid_in(valid_in), .sel(sel),
             // Inputs
                //PROC
-            .addr    (Addr),
-            .data    (DataIn),
+            .addr(Addr),
+            .data(DataIn),
             .rd(Rd),
             .wr(Wr),
                //MEM
-            .stall   (stall),
+            .stall   (Stall),
             .busy    (busy),
                //CACHE
             .hit(hit),
             .dirty(dirty),
-            .valid(valid),
+            .valid(valid)
             );
 
 
@@ -80,7 +83,7 @@ module mem_system(/*AUTOARG*/
 
    four_bank_mem mem(// Outputs
                      .data_out          (mem_data_out),
-                     .stall             (stall),
+                     .stall             (Stall),
                      .busy              (busy),
                      .err               (),
                      // Inputs
@@ -94,6 +97,7 @@ module mem_system(/*AUTOARG*/
    
    // your code here
 
+   assign dataOut = sel ? mem_data_out : cache_data_out;
    
 endmodule // mem_system
 `default_nettype wire
