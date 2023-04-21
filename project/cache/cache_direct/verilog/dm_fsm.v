8cb880a1661042e6c337a77c25ca43911840dc4d
Timo Diep <tdiep@wisc.edu>
2023-04-21 13:19:14 -0500

    More states

--- project/cache/cache_direct/verilog/dm_fsm.v
+++ project/cache/cache_direct/verilog/dm_fsm.v
@@ -46,11 +46,11 @@ module dm_fsm(  // Outputs
     assign cache_index = addr[10:3];
     assign cache_tag = addr[2:0]; 
 
-    reg [3:0] state;
+    reg [3:0] state, nxt_state;
     /* State list
     0x0/default = rst/idle state
     0x1 = Check cache
-
+    0x2 = Cache hit
 
 
     */
@@ -62,12 +62,25 @@ module dm_fsm(  // Outputs
         comp = 1'b0;
         case(state)
             default: begin // Default/Idle case
-                state = rd | wr ?  4'h1 : 4'h0;
+                nxt_state = rd | wr ?  4'h1 : 4'h0;
             end
+            
             4'h1: begin // Check cache  
                 cache_en = 1'b1;
                 comp = 1'b1;
+
+                nxt_state = hit ? /*hit case*/ : /* miss case*/;
+            end
+
+            4'h2: begin // Cache hit
+                nxt_state = rd ? 4'h3 : (wr ? /*Write case*/ : 4'h0); 
+            end
+
+            4'h3: begin // Read Cache
+
             end
         endcase 
     end 
+
+    dff stateReg(.q(state), d(nxt_state), clk(clk), rst(rst));
 endmodule
